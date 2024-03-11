fe8238de5af59430e79e76a0fde0354790225caf
Nguyen Minh Duc <nguyenminhduc24396@gmail.com>
2024-03-11 17:20:01 +0700

    aaaa

--- monthly_challenges/settings.py
+++ monthly_challenges/settings.py
@@ -56,7 +56,6 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            # BASE_DIR / 'challenges' / 'templates',
             BASE_DIR / "templates"
         ],
         'APP_DIRS': True,
@@ -120,6 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
